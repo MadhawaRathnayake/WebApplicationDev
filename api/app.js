84346c287d4d77bedd7f076b4227c4385dfc3bfd
Madhawa Rathnayake <95226002+MadhawaRathnayake@users.noreply.github.com>
2024-07-31 18:22:13 +0530

    Update app.js cors allowedHeaders

--- api/app.js
+++ api/app.js
@@ -10,7 +10,10 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({origin:"https://easycar-client.vercel.app", credentials: true}));
+app.use(cors({origin: "https://easycar-client.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
