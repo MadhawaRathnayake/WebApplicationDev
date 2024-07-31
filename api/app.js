33c9e03fe7f49bb0a141b0a1c49fc67dfa193a93
Madhawa Rathnayake <95226002+MadhawaRathnayake@users.noreply.github.com>
2024-07-31 18:55:11 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({origin: "https://easycar-client.vercel.app/"}));
+app.use(cors({origin: "*"}));
 app.use(express.json());
 app.use(cookieParser());
 
