67f1a69ced0f3fb1f4b6bcd60ffbd44206e019ae
Madhawa Rathnayake <95226002+MadhawaRathnayake@users.noreply.github.com>
2024-07-31 12:39:41 +0530

    Update app.js

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io"
 
 const io = new Server({
     cors: {
-        origin: "https://easycar-client.vercel.app/",
+        origin: "https://easycar-client.vercel.app",
     },
 });
 
