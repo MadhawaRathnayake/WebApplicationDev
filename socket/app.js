4de7accacd8aae1827acbf398e74e130b54aca78
Madhawa Rathnayake <95226002+MadhawaRathnayake@users.noreply.github.com>
2024-07-31 13:01:18 +0530

    Update app.js

--- socket/app.js
+++ socket/app.js
@@ -1,43 +1,48 @@
-import { Server } from "socket.io"
+import { Server } from "socket.io";
+import http from "http";
 
-const io = new Server({
-    cors: {
-        origin: "https://easycar-client.vercel.app",
-    },
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: "https://easycar-client.vercel.app",
+  },
 });
 
 let onlineUsers = [];
 
 const addUser = (userId, socketId) => {
-    const userExcist = onlineUsers.find((user) => user.userId === userId);
-    if (!userExcist) {
-        onlineUsers.push({ userId, socketId })
-    }
-}
+  const userExists = onlineUsers.find((user) => user.userId === userId);
+  if (!userExists) {
+    onlineUsers.push({ userId, socketId });
+  }
+};
 
 const removeUser = (socketId) => {
-    onlineUsers = onlineUsers.filter(user => user.socketId !== socketId)
-}
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
 
 const getUser = (userId) => {
-    return onlineUsers.find(user => user.userId === userId)
-}
+  return onlineUsers.find((user) => user.userId === userId);
+};
 
 io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log(onlineUsers);
+  });
 
-    socket.on("newUser", (userId) => {
-        addUser(userId, socket.id);
-        console.log(onlineUsers);
-    });
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
 
-    socket.on("sendMessage", ({ receiverId, data }) => {
-        const receiver = getUser(receiverId)
-        io.to(receiver.socketId).emit("getMessage", data);
-    });
-
-    socket.on("disconnect", () => {
-        removeUser(socket.id);
-    });
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
 });
 
-io.listen(process.env.PORT || "4000");
+server.listen(process.env.PORT || 4000, () => {
+  console.log("Socket.io server is running");
+});
+
+export default server;
