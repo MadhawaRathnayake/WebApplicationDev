3b1190aa9b9bf350943b811f2c06bf8144b0933e
Madhawa Rathnayake <95226002+MadhawaRathnayake@users.noreply.github.com>
2024-07-31 11:33:09 +0530

    Update apiRequests.js base url

--- client/src/lib/apiRequests.js
+++ client/src/lib/apiRequests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8800/api",
+    baseURL:"https://easycar-backend.vercel.app:8800/api",
     withCredentials:true,
 })
 
